cb32301ca6296334fcd3d89e7a3646cf2748c516
sed-int <nouveau0309@gmail.com>
2022-10-12 23:48:39 +0900

    1780

---
+++ Recursion/1780_종이의_개수.cpp
@@ -0,0 +1,40 @@
+// Authored by : cpprhtn
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/b8488e82105d49e89ca6f39fd8ee665b
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int paper[2200][2200];
+int cnt[3];
+
+bool check(int x, int y, int n) {
+  for (int i = x; i < x + n; i++)
+  	for (int j = y; j < y + n; j++)
+   		if (paper[x][y] != paper[i][j]) return false;
+  return true;
+}
+
+void solve(int x, int y, int z)
+{
+  if (check(x, y, z)) {
+    cnt[paper[x][y] + 1] += 1;
+    return;
+  }
+  int n = z / 3;
+  for (int i = 0; i < 3; i++)
+  	for (int j = 0; j < 3; j++)
+    	solve(x + i * n, y + j * n, n);
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> N;
+  for (int i = 0; i < N; i++)
+  	for (int j = 0; j < N; j++)
+    	cin >> paper[i][j];
+  solve(0, 0, N);
+  for (int i = 0; i < 3; i++) cout << cnt[i] << "\n";
+}
