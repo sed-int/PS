49174f8098ae81915a22cdc33aedf987254d54c6
sed-int <nouveau0309@gmail.com>
2022-12-07 19:00:28 +0900

    dp

---
+++ DP/10844_쉬운_계단수.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int div = 1000000000;
+int n, d[102];
+int main(void){
+    cin >> n;
+    d[1] = 9;
+    d[2] = 17;
+    for (int i = 3; i <= n; i++){
+        d[i] = (d[i-1] * 2 - 1)%div;
+    }
+    cout << d[n];
+}
