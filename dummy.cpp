01cdcea645900f71d3f79bf60a178017c21e7b7a
sed-int <nouveau0309@gmail.com>
2022-12-21 13:46:08 +0900

    decluttering

--- dummy.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    unsigned int a = 0x12345678, b = 0x11223344, c = 0x11223344;
-    b = (unsigned short int)a;
-    c = (unsigned char)a;
-    printf("a=%x, b = %x, c = %x\n", a, b, c);
-}
